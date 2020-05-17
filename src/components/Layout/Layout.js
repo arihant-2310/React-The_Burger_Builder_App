80fca54a8e220e1cca78c608ffa21f8c4336bad3
arihant-2310 <arihant2310@gmail.com>
2020-05-17 16:21:44 +0530

    adds burger component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
+import classes from './Layout.css';
 
 const layout = (props) => (
 
     <Auxiliary>
         <div>ToolBar,Sidedrawer,Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxiliary>
