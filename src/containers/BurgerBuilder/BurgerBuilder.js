f833800d149091fdb96775c542c8db1f83c34324
arihant-2310 <arihant2310@gmail.com>
2020-05-19 22:01:33 +0530

    adds backdrop to order summary modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,6 +23,9 @@ class BurgerBuilder extends Component{
         purchasable:false,
         purchasing:false,
     }
+    purchaseCancelHandler = () => {
+        this.setState({purchasing:false});
+    }
     purchaseHandler = () => {
         this.setState({purchasing:true})
     }
@@ -76,7 +79,7 @@ class BurgerBuilder extends Component{
         }
         return (
             <Auxiliary>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
