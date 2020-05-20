da4c1ca62d4c66b7c1d2f051efb143521492b44e
arihant-2310 <arihant2310@gmail.com>
2020-05-20 19:08:54 +0530

    adds side drawer close functionality

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
+class Layout extends Component {
 
-    <Auxiliary>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxiliary>
-    
-);
+    state = {
+        showSideDrawer:true,
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer:false});
+    }
+
+    render(){
+        return (
+        <Auxiliary>
+            <Toolbar />
+            <SideDrawer 
+                open= {this.state.showSideDrawer}
+                closed={this.sideDrawerClosedHandler}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Auxiliary>
+        )
+    } 
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import classes from './SideDrawer.css';
+import Auxiliary from '../../../hoc/Auxiliary';
  const sideDrawer = (props) => {
 
-
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return(
-        <div className={classes.SideDrawer}>
+        <Auxiliary>
+        <Backdrop show={props.open} clicked={props.closed}/>
+        <div className={attachedClasses.join(' ')}>
             <div className={classes.Logo}>
                 <Logo />
             </div>
@@ -14,6 +21,7 @@ import classes from './SideDrawer.css';
                 <NavigationItems />
             </nav>
         </div>
+        </Auxiliary>
     );
  };
 
